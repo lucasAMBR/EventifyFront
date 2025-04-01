d46063cb31326072227ab88e8703964c3fdc955e
Lucas Ambrosio <lucas.silvaambrosio_9119@outlook.com>
2025-04-01 15:50:02 -0300

    Projeto base e exemplo de login

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [emailInput, setEmailInput] = useState("")
+  const [passwordInput, setPasswordInput] = useState("")
+
+  const [loggedUser, setLoggedUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const handleEmailChange = (e) => {
+    setEmailInput(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPasswordInput(e.target.value);
+  }
+
+
+  const handleLogin = async(event) => {
+      event.preventDefault();
+
+      setErrorMessage(null)
+
+      const formData = new FormData();
+      formData.append("email", emailInput);
+      formData.append("password", passwordInput);
+
+      try{
+        const response = await fetch("http://localhost:8080/api/auth/login", {
+          method: "POST",
+          body:formData
+        })
+
+        if(response.ok){
+          const data = await response.json();
+          console.log(data)
+          setLoggedUser(data);
+        }else{
+          const data = await response.json();
+          setErrorMessage(data);
+        }
+
+      }catch(error){
+          console.log(error);
+      }
+
+  }
+
+  return (
+    <>
+      <form onSubmit={handleLogin}>
+        {errorMessage != null ? <div>Erro: {errorMessage.message}</div> : ""}
+        <input type="text" placeholder="email" value={emailInput} onChange={handleEmailChange}/>
+        <input type="password" placeholder="senha" value={passwordInput} onChange={handlePasswordChange}/>
+        <button type="submit">Login</button>
+      </form>
+      {loggedUser != null && <h2>{loggedUser.name}</h2>}
+    </>
+  )
+}
+
+export default App
