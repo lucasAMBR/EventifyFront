8b7ef24e6f42fc8dd79c6dce8f5e4b52ef3bfee3
Carlos <calberto766@yahoo.com.br>
2025-04-09 18:51:09 -0300

    Aba Eventos finalizada

--- src/pages/events/Events.jsx
+++ src/pages/events/Events.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Events.css';
 import Search from './components/Search';
+import Content from './components/Content';
 
 const Events = () => {
   return (
     <div className="EventsPage">
       <Search/>
-      {}
+      <Content/>
     </div>
   );
 };
