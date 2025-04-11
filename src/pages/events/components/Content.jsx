e76504178559201838b483877cdf001c325c4e4f
Lucas Ambrosio <lucas.silvaambrosio_9119@outlook.com>
2025-04-11 11:23:17 -0300

    Conexão com a API buscar eventos

--- src/pages/events/components/Content.jsx
+++ src/pages/events/components/Content.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Events.css';
 
 const Content = () => {
   const popularEvents = [1, 2, 3]; /*Array de repeticao */
-  const otherEvents = [5, 6, 7, 8,9,10,11,12];
+  const otherEvents = [5, 6, 7, 8, 9, 10, 11, 12];
+
+  const [ eventList, setEventList ] = useState([]); /*Armazenar e manipular os dados dos eventos*/
+
+  
+  useEffect (() => {
+    handlePostList()
+  }, [])
+
+  /*Fazer a requisição para o back,
+   deixa async para */
+  const handlePostList = async() => {
+    try{
+      const response = await fetch("http://localhost:8080/api/event/list/user/1")
+      
+      if(response.ok){
+        const data = await response.json(); //Para o sistema ler a resposta
+        console.log(data)
+        setEventList(data)
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div className="content-container">
@@ -11,17 +35,18 @@ const Content = () => {
       <section className="section">
         <h2 className="section-title">Most Popular events now</h2>
         <div className="card-container">
-          {popularEvents.map((item, index) => (
+          {eventList.length == 0 && "SEM EVENTOS"}
+          {eventList.map((item, index) => (
             <div className="event-card" key={`popular-${index}`}>
               <div className="event-image">
                 <img src="/test.png" alt="evento popular" className="event-img" />
-                <span className="presential-badge">Presential</span>
+                <span className="presential-badge">{item.type}</span>
               </div>
               <div className="event-info">
-                <h3>Lorem ipsum</h3>
-                <div className="detail"><img src="/time.svg" className="iconCard" /> 25/10, 22:30</div>
-                <div className="detail"><img src="/location.svg" className="iconCard" /> São Paulo, SP, Brasil</div>
-                <div className="detail"><img src="/person.svg" className="iconCard" /> Limite: 15.000 pessoas</div>
+                <h3>{item.title}</h3>
+                <div className="detail"><img src="/time.svg" className="iconCard" />{`${item.date[2]}/${item.date[1]}/${item.date[0]}, ${item.hour[0]}:${item.hour[1]}`}</div>
+                <div className="detail"><img src="/location.svg" className="iconCard" />{item.location}</div>
+                <div className="detail"><img src="/person.svg" className="iconCard" /> Limite: {item.guestLimit}</div>
                 <button>More Details</button>
               </div>
             </div>
